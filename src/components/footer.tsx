192c9e81dbcd3249ab65c7d9d8913b2c3da7d42d
Tristan <tristangee@gmail.com>
2024-08-09 11:12:21 -0700

    insta link

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -5,7 +5,7 @@ export default function Footer() {
     return (
         <div className="bg-black flex justify-center items-center p-20 h-[15rem] w-full gap-4">
             <Link href="mailto:fremontinspire@gmail.com"><IconMail size={36} className="text-white" /></Link>
-            <Link href=""><IconBrandInstagram size={36} className="text-white" /></Link>
+            <Link href="https://www.instagram.com/fremontinspiredyouth/"><IconBrandInstagram size={36} className="text-white" /></Link>
         </div>
     )
 }
